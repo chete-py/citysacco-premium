68343e9b0f2f09c15a7757aa24e1b654d071c275
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2025-02-07 17:29:19 +0300

    Update citysacco.py

--- citysacco.py
+++ citysacco.py
@@ -49,7 +49,7 @@ with view1:
         formatted_apa_gross_premium = 'NA'
         formatted_apa_premium = 'NA'
         formatted_apa_levies = 'NA'
-        formatted_apa_total = 'NA'
+        formatted_apa_total = 'Consider TPO'
         formatted_apa_car_hire = 'NA'
     else:
         if 500000 < value < 2500000:
